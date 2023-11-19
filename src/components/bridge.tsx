e131c5e05630833834e4d9ae2535a61e48a09eb2
eddy <be@be-technology.fr>
2023-11-19 11:01:20 +0300

    send from scroll

--- src/components/bridge.tsx
+++ src/components/bridge.tsx
@@ -1,30 +1,81 @@
-import * as React from 'react'
-import { useDebounce } from 'use-debounce'
-import { usePrepareSendTransaction, useSendTransaction } from 'wagmi'
-import { parseEther } from 'viem'
+import { useEffect, useState } from 'react';
+import { useDebounce } from 'use-debounce';
+import { Address, encodeAbiParameters, parseEther } from 'viem';
+import {
+	useAccount,
+	useContractWrite,
+	usePrepareContractWrite,
+	useWaitForTransaction,
+
+} from 'wagmi';
 
 export default function Bridge() {
 
-	const [to, setTo] = React.useState('0x932f80fc3d023e8dac12a3ae2a8611fdd3cf360f')
+	const scrollAdr = "0x3808d0F2F25839E73e0Fbf711368fC4aE80c7763";
+	const zkEvmAdr = "";
+	const lineaAdr = "0x711A70b4e2af3388a0E4061e53AAd2267439270D";
+	const l1BridgeAdr = "0x932f80fc3d023e8dac12a3ae2a8611fdd3cf360f";
+
+	//const [{ data: signData }, getSigner] = useSigner();
+	const { address, connector, isConnected } = useAccount()
+
+	const [from, setFrom] = useState<string>(scrollAdr)
+	const [debouncedFrom] = useDebounce(from, 500)
+
+	const [account, setAccount] = useState<Address>(address || "0xDBf0DC3b7921E9Ef897031db1DAe239B4E45Af5f")
+	const [debouncedAccount] = useDebounce(account, 500)
+
+	const [to, setTo] = useState<string>("59140")
 	const [debouncedTo] = useDebounce(to, 500)
 
-	const [amount, setAmount] = React.useState('')
+	const [amount, setAmount] = useState('0.01')
 	const [debouncedAmount] = useDebounce(amount, 500)
 
-	const { config } = usePrepareSendTransaction({
-		to: debouncedTo,
-		value: debouncedAmount ? parseEther(debouncedAmount) : undefined,
-	})
+	const [encode, setEncode] = useState<string>("")
 
-	const { sendTransaction } = useSendTransaction(config);
+	useEffect(() => {
+		let val = encodeAbiParameters(
+			[
+				{ name: 'chainId', type: 'uint64' },
+				{ name: 'user', type: 'address' }
+			],
+			[BigInt(debouncedTo), debouncedAccount]
+		);
+		setEncode(val);
+	}, [account, debouncedTo]);
+
+	const { config,
+		error: prepareError,
+		isError: isPrepareError } = usePrepareContractWrite({
+			address: debouncedFrom as any,
+			abi: [
+				{
+					name: 'sendMessage',
+					type: 'function',
+					stateMutability: 'payable',
+					inputs: [{ "type": "address", "name": "_to", "internalType": "address" },
+					{ "type": "uint256", "name": "_value", "internalType": "uint256" },
+					{ "type": "bytes", "name": "_message", "internalType": "bytes" },
+					{ "type": "uint256", "name": "_gasLimit", "internalType": "uint256" }],
+					outputs: [],
+				},
+			],
+			functionName: 'sendMessage',
+			args: [l1BridgeAdr, parseEther(debouncedAmount), encode, 1000000],
+			value: parseEther(debouncedAmount),
+			
+		});
+	const { data, error, isError, write } = useContractWrite(config);
+
+	const { isLoading, isSuccess } = useWaitForTransaction({
+		hash: data?.hash,
+	})
 
 	const validate = async () => {
 
-		sendTransaction?.();
-		setTimeout(() => {
-			alert("Token submit, wait 30 min to get it an other chain")
-		}, 10000);
-		
+		console.log("debouncedAmount", parseEther(debouncedAmount));
+		write?.();
+		console.log("write", write);
 	};
 
 
@@ -32,21 +83,18 @@ export default function Bridge() {
 		<div>
 			<div className="zk-form">
 				<label>From : </label>
-				<select>
-					<option value="59140">
-						Linea
-					</option>
-					<option value="534353">
+				<select value={from} onChange={(ev) => setFrom(ev.target.value)}>
+					<option value={scrollAdr}>
 						Scroll
 					</option>
-					<option value="1442">
+					<option value={zkEvmAdr}>
 						Polygon ZkEvm
 					</option>
 				</select>
 			</div>
 			<div className="zk-form">
 				<label>To : </label>
-				<select>
+				<select value={to} onChange={(ev) => setTo(ev.target.value)}>
 					<option value="59140">
 						Linea
 					</option>
@@ -60,8 +108,27 @@ export default function Bridge() {
 			</div>
 			<div className="zk-form">
 				<label>Amount : </label>
-				<input type="number" value={amount} onChange={(ev) => setAmount(ev.currentTarget.value)} ></input>
+				<input type="number" value={amount} onChange={(ev) => setAmount(ev.target.value)} ></input>
 			</div>
+
+			<div className="zk-form">
+				<label>Receiver : </label>
+				<input type="text" value={account} onChange={(ev) => setAccount(ev.target.value)} ></input>
+			</div>
+
+
+			{isSuccess && (
+				<div>
+					Successfully sent {amount} ether to {to}
+					<div>
+						<a>Tx id, wait 30 min to finish bridge : {data?.hash}</a>
+					</div>
+				</div>
+			)}
+
+			{(isPrepareError || isError) && (
+				<div>Error: {(prepareError || error)?.message}</div>
+			)}
 
 			<div className="zk-form" style={{ justifyContent: 'center' }}>
 				<button onClick={(e) => {
