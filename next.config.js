10463025dd5df85bcf4eb9fbe8e67f7c8f699dd3
eddy <be@be-technology.fr>
2023-11-19 08:07:57 +0300

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/frontend-evm/' : '', // update if your repo name changes for 'npm run deploy' to work successfully
+
 };
 
 module.exports = nextConfig;
